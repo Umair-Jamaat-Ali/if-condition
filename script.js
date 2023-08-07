d22ae24566fcb9ca5a69be2f4c25fb512a2b81aa
Umair Jamaat <umairansari758@gmail.com>
2023-08-07 23:43:15 +0500

    if-condition

---
+++ script.js
@@ -0,0 +1,42 @@
+// var userMarks =+ prompt("enter a number");
+
+// if (userInput % 2 == 0) {
+//     console.log("this is even number");
+// } else{
+//     console.log("this is odd number");
+// }
+
+// if(userMarks <= 40){
+//     console.log("you are failed!");
+// }
+// else if(userMarks <= 50){
+//     console.log("your grades are E");
+// } 
+// else if(userMarks <= 60){
+//     console.log("your grades are D");
+// }
+// else if (userMarks <= 70) {
+//     console.log("your grades are C");
+// }
+// else if (userMarks <= 80) {
+//     console.log("your grades are B");
+// }
+// else if (userMarks <= 90) {
+//     console.log("your grades are A");
+// }
+// else if (userMarks <= 100) {
+//     console.log("your grades are A+");
+// }
+
+
+if (4 < 6 && 5 == 5) {
+    console.log("both conditins are true");
+} else {
+    console.log("both conditins are not true");
+}
+
+if (4 > 6 || 5 == 5) {
+    console.log("OR operater");
+} else {
+    console.log("both conditins are not true");
+}
